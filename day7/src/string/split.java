8caf740536dc8ed897b46e33abca09674cb93674
Nero-Number-One <2572591652@qq.com>
2022-05-03 21:47:40 +0800

    note

--- day7/src/string/split.java
+++ day7/src/string/split.java
@@ -17,7 +17,7 @@ public class split {
 
         System.out.println("================");
         String str3 = "XXX.YYY.ZZZ";
-        String[] arr3 = str3.split("."); //不能使用 . 来切割
+        String[] arr3 = str3.split("\\."); //不能使用 . 来切割，若要使用，需要使用 \\.
         System.out.println(arr3.length);
         for (int i = 0; i < arr3.length; i++) {
             System.out.println(arr3[i]);
