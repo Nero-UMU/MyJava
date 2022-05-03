c0622cdb5ea5b565b5111f8ae4164a0b116d29fd
Nero-Number-One <2572591652@qq.com>
2022-05-03 21:46:47 +0800

    note

--- day7/src/string/convert.java
+++ day7/src/string/convert.java
@@ -2,6 +2,23 @@ package string;
 
 public class convert {
     public static void main(String[] args) {
+        char[] chars = "Hello".toCharArray(); //转换为字符数组
+        System.out.println(chars[0]);
+        System.out.println(chars.length);
 
+        byte[] bytes = "abc".getBytes(); //转换为字节数组
+        for (int i = 0; i < bytes.length; i++) {
+            System.out.println(bytes[i]);
+        }
+
+        String str1 = "How are you?";
+        String str2 = str1.replace("o", "*"); //字符替换
+        System.out.println(str1);
+        System.out.println(str2);
+
+        String lang1 = "fuck you!";
+        String lang2 = lang1.replace("uc", "**");
+        System.out.println(lang1);
+        System.out.println(lang2);
     }
 }
---
+++ day7/src/string/split.java
@@ -0,0 +1,26 @@
+package string;
+
+public class split {
+    public static void main(String[] args) {
+        String str1 = "aaa,bbb,ccc,ddd";
+        String[] arr1 = str1.split(",");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(arr1[i]);
+        }
+
+        System.out.println("================");
+        String str2 = "aaa bbb ccc ddd eee";
+        String[] arr2 = str2.split(" ");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]);
+        }
+
+        System.out.println("================");
+        String str3 = "XXX.YYY.ZZZ";
+        String[] arr3 = str3.split("."); //不能使用 . 来切割
+        System.out.println(arr3.length);
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.println(arr3[i]);
+        }
+    }
+}
