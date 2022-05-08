5cd4bde1136a3db603d38f5ece94e3f08b3306cc
Nero-Number-One <2572591652@qq.com>
2022-05-08 10:59:41 +0800

    note

--- day10/src/jubuneibulei/Outer.java
+++ day10/src/jubuneibulei/Outer.java
@@ -2,11 +2,26 @@ package jubuneibulei;
 
 /*
 如果一个类是定义在方法内部的，那么这是一个局部内部类
-“局部”：只有房钱所属的方法才能使用它，除了这个方法就不能用了
+“局部”：只有当前所属的方法才能使用它，出了这个方法就不能用了
 格式：
 修饰符 class 外部类名称{
+    修饰符 返回值类型 外部类方法名称(参数列表){
+        class 局部内部类名称{
+        }
+    }
 }
+就是一个方法里面定义了一个类，方便在这个方法内使用，别的地方都不能使用这个类
  */
 
 public class Outer {
+    public void methodOuter(){
+        class inner{
+            int num = 10;
+            public void methodInner(){
+                System.out.println(num);
+            }
+        }
+        inner in = new inner();
+        in.methodInner();
+    }
 }
