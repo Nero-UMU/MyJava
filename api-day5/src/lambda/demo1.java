18a380ffa1b3874c1aace9a5506a9e4b87844494
nero <2572591652@qq.com>
2022-05-23 00:37:25 +0800

    note

---
+++ api-day5/src/lambda/demo1.java
@@ -0,0 +1,18 @@
+package lambda;
+
+/*
+Lambda表达式
+格式:
+(参数列表) -> {一些重写方法的代码};
+(): 接口找那个抽象方法的参数列表，没有参数就空着，有参数就写出参数，多个参数使用逗号分隔
+->: 传递的意思，把参数传递给方法体{}
+{}: 重写接口的抽象方法的方法体
+ */
+
+public class demo1 {
+    public static void main(String[] args) {
+        new Thread(()->{
+            System.out.println(Thread.currentThread().getName() + "新线程创建了");
+        }).start();
+    }
+}
