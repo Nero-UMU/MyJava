8156f0718e3e588f0476988b687946961b2ec6ba
nero <2572591652@qq.com>
2022-05-29 22:36:17 +0800

    note

--- api-day6/src/inputStream/demo2.java
+++ api-day6/src/inputStream/demo2.java
@@ -1,20 +1,29 @@
 package inputStream;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
 public class demo2 {
     public static void main(String[] args) throws IOException {
-        FileInputStream fis = new FileInputStream("justTest.md");
+        FileInputStream fis1 = new FileInputStream("justTest.md");
         byte[] br = new byte[2]; //用来存储读取出来的字节
-        int len = fis.read(br); //len表示有效读取字节个数
+        int len = fis1.read(br); //len表示有效读取字节个数
         System.out.println(len);
         System.out.println(Arrays.toString(br));
         System.out.println(new String(br));
 
-        len = fis.read(br); //读取多个字节，并存入br中
+        len = fis1.read(br); //读取多个字节，并存入br中
         System.out.println(Arrays.toString(br));
-        fis.close();
+        fis1.close();
+
+        FileInputStream fis2 = new FileInputStream("justTest.md");
+        byte[] bytes = new byte[1024];
+        len = 0;
+        while ((len = fis2.read(bytes)) != -1){
+            System.out.println(new String(bytes, 0, len));
+        }
+
     }
 }
