25f5873d56d0a80d3518501d76b50118f93152a5
nero <2572591652@qq.com>
2022-06-08 22:29:45 +0800

    note

--- api-day8/src/functionalInterface/supplier.java
+++ api-day8/src/functionalInterface/supplier.java
@@ -1,7 +1,15 @@
 package functionalInterface;
 
+import java.util.function.Supplier;
+
 public class supplier {
     public static void main(String[] args) {
-
+        String str = getString(()->{
+            return "haha";
+        });
+        System.out.println(str);
+    }
+    public static String getString(Supplier<String> sup){
+        return sup.get();
     }
 }
