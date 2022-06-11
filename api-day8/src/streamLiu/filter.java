7cfef36b057bbec63f6d077280ba6d615bab3056
nero <2572591652@qq.com>
2022-06-11 21:22:59 +0800

    note

---
+++ api-day8/src/streamLiu/filter.java
@@ -0,0 +1,21 @@
+package streamLiu;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class filter {
+    public static void main(String[] args) {
+        ArrayList<String> str = new ArrayList<>();
+        str.add("w t f");
+        str.add("fuck me");
+        str.add("what the hell");
+        str.add("surprise mother fucker");
+        Stream<String> stream = str.stream();
+        Stream<String> stream1 = stream.filter((name) -> {return name.startsWith("w");});
+        stream1.forEach(System.out::println);
+//        stream.forEach((name)->{
+//            System.out.println(name);
+//        });
+        //stream流只能用一次
+    }
+}
---
+++ api-day8/src/streamLiu/forEach.java
@@ -0,0 +1,11 @@
+package streamLiu;
+
+import java.util.stream.Stream;
+
+public class forEach {
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("aaa", "bbb", "ccc", "ddd");
+        stream.forEach((name)-> System.out.println(name)
+        );
+    }
+}
