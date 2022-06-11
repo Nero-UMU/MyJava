8172635c35caef2bc6a9281bb5c6e245ad5a70d3
nero <2572591652@qq.com>
2022-06-11 22:24:01 +0800

    note

---
+++ api-day8/src/streamLiu/limit.java
@@ -0,0 +1,13 @@
+package streamLiu;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class limit {
+    public static void main(String[] args) {
+        String[] arr = new String[]{"hello", "world", "cao", "wlg"};
+        Stream<String> stream = Arrays.stream(arr);
+        Stream<String> stream1 = stream.limit(3);
+        stream1.forEach((name)-> System.out.println(name));
+    }
+}
---
+++ api-day8/src/streamLiu/skip.java
@@ -0,0 +1,13 @@
+package streamLiu;
+
+import java.util.stream.Stream;
+
+public class skip {
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("草", "靠", "可恶", "我去");
+        Stream<String> stream1 = stream.skip(2);
+        stream1.forEach((name)->{
+            System.out.println(name);
+        });
+    }
+}
