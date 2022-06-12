fa004fcd1ce18c552bc52ca84f244ef7022fb52d
nero <2572591652@qq.com>
2022-06-12 22:15:55 +0800

    note

--- api-day8/src/streamLiu/count.java
+++ api-day8/src/streamLiu/count.java
@@ -5,15 +5,7 @@ import java.util.stream.Stream;
 
 public class count {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        Stream<Integer> stream = list.stream();
+        Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 6);
         long count = stream.count();
         System.out.println(count);
     }
--- api-day8/src/streamLiu/forEach.java
+++ api-day8/src/streamLiu/forEach.java
@@ -5,7 +5,8 @@ import java.util.stream.Stream;
 public class forEach {
     public static void main(String[] args) {
         Stream<String> stream = Stream.of("aaa", "bbb", "ccc", "ddd");
-        stream.forEach((name)-> System.out.println(name)
-        );
+        stream.forEach((name)-> {
+            System.out.println("str:" + name);
+        });
     }
 }
